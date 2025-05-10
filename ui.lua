bc8b3cd622e3930675732932e00932f771cd1fae
ringtaa <eeeiqjjj50@gmail.com>
2025-05-10 11:50:01 -0400

    Update ui.lua

--- ui.lua
+++ ui.lua
@@ -473,7 +473,7 @@ CreateButton(FeaturesTab, "Fly", function()
     
 end, UDim2.new(0.1, 0, 0.06, 0))
 
-CreateButton(BypassTab, "Fly Off", function()
+CreateButton(FeaturesTab, "Fly Off", function()
     loadstring(game:HttpGet("https://github.com/ringtaa/unfly.github.io/blob/main/unfly.lua"))()
 end, UDim2.new(0.1, 0, 0.53, 0))
 
